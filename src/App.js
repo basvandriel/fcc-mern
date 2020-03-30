dcca0ebc3a0d512f07987d854927ae8b1f91deeb
Bas <basvandriel94@gmail.com>
2020-03-30 18:01:26 +0200

    start of frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
+import Navbar from './components/navbar';
+import ExercisesList from './components/exercises-list';
+import EditExercise from './components/edit-exercise';
+import CreateExercise from './components/create-exercise';
+import CreateUser from './components/create-user';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Navbar/>
+                <img src={logo} className="App-logo" alt="logo"/>
+
+                <Route path="/" component={ExercisesList}/>
+                <Route path="/edit/:id" component={EditExercise}/>
+                <Route path="/create" component={CreateExercise}/>
+                <Route path="/user/create" component={CreateUser}/>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+                <Link to="/" className="navbar-brand">Exercise Tracker</Link>
+                <div className="collpase navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="navbar-item">
+                            <Link to="/" className="nav-link App-link">Exercises</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/create" className="nav-link App- link">Create Exercise Log</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/user/create" className="nav-link App-link">Create User</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
